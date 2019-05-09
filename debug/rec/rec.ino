2a21b5ce551f2b461b09c1c0dc141255d9869142
ConNoa <constantin.noack@uni-weimar.de>
2019-05-10 01:53:52 +0200

    433

--- debug/rec/rec.ino
+++ debug/rec/rec.ino
@@ -1,77 +1,29 @@
-/*
-Created by Yvan / https://Brainy-Bits.com
+#include <RCSwitch.h>
 
-This code is in the public domain...
-You can: copy it, use it, modify it, share it or just plain ignore it!
-Thx!
+RCSwitch mySwitch = RCSwitch();
 
-*/
-
-
-// NRF24L01 Module Tutorial - Code for Receiver using Arduino UNO
-
-//Include needed Libraries at beginning
-#include "nRF24L01.h" // NRF24L01 library created by TMRh20 https://github.com/TMRh20/RF24
-#include "RF24.h"
-#include "SPI.h"
-#include "FastLED.h" // FastLED library for WS2812 RGB Stick http://fastled.io/
-
-#define NUM_LEDS 228 // Number of leds on stick
-#define LED_PIN 3 // Digital In (DI) of RGB Stick connected to pin 8 of the UNO
-
-CRGB leds[NUM_LEDS]; // FastLED Library Init
-
-int ReceivedMessage[1] = {000}; // Used to store value received by the NRF24L01
-
-RF24 radio(7,8); // NRF24L01 used SPI pins + Pin 9 and 10 on the UNO
-
-const uint64_t pipe = 0xE6E6E6E6E6E6; // Needs to be the same for communicating between 2 NRF24L01
-
-
-void setup(void){
-
-FastLED.addLeds<NEOPIXEL,LED_PIN>(leds, NUM_LEDS); // Setup FastLED Library
-FastLED.clear(); // Clear the RGB Stick LEDs
-
-// Light up starting LED's
-for(int x = 0; x != NUM_LEDS; x++) {
-leds[x] = CRGB::Red;
-}
-
-FastLED.setBrightness(50);
-FastLED.show();
-
-radio.begin(); // Start the NRF24L01
-
-radio.openReadingPipe(1,pipe); // Get NRF24L01 ready to receive
-
-radio.startListening(); // Listen to see if information received
-
-pinMode(LED_PIN, OUTPUT); // Set RGB Stick UNO pin to an OUTPUT
-}
-
-void loop(void){
-
-while (radio.available())
+void setup()
 {
-radio.read(ReceivedMessage, 1); // Read information from the NRF24L01
+  Serial.begin(9600);
+  mySwitch.enableReceive(0);  // Empfänger ist an Interrupt-Pin "0" - Das ist am UNO der Pin2
+}
 
-if (ReceivedMessage[0] == 111) // Indicates switch is pressed
-{
-for(int x = 0; x != NUM_LEDS; x++)
-{
-leds[x] = CRGB::Green;
-FastLED.show();
-}
-}
-else
-{
-for(int x = 0; x != NUM_LEDS; x++)
-{
-leds[x] = CRGB::Red;
-FastLED.show();
-}
-}
-delay(10);
-}
+void loop() {
+  if (mySwitch.available()) // Wenn ein Code Empfangen wird...
+  {
+    int value = mySwitch.getReceivedValue(); // Empfangene Daten werden unter der Variable "value" gespeichert.
+
+    if (value == 0) // Wenn die Empfangenen Daten "0" sind, wird "Unbekannter Code" angezeigt.
+    {
+      Serial.println("Unbekannter Code");
+    }
+
+    else // Wenn der Empfangene Code brauchbar ist, wird er hier an den Serial Monitor gesendet.
+    {
+      Serial.print("Empfangen: ");
+      Serial.println( mySwitch.getReceivedValue() );
+    }
+
+    mySwitch.resetAvailable(); // Hier wird der Empfänger "resettet"
+  }
 }
